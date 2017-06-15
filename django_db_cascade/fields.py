529648d27238d49f611bacefcc0a07322496a578
NickStefan <nickstefan12@gmail.com>
2017-06-15 14:55:13 -0700

    debug

--- django_db_cascade/fields.py
+++ django_db_cascade/fields.py
@@ -3,6 +3,7 @@ from django.db.models import ForeignKey as FK
 class ForeignKey(FK):
     def __init__(self, *args, db_cascade=False, **kwargs):
         self.db_cascade = db_cascade
+        print('fk', self.db_cascade)
         kwargs.pop('db_cascade', None)
         super().__init__(*args, **kwargs)
 
