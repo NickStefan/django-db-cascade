9c41fdb4d1bbf40eb9938d2d77c964a86e6a643e
NickStefan <nickstefan12@gmail.com>
2017-06-15 13:38:41 -0700

    unexpected kwarg

--- django_db_cascade/fields.py
+++ django_db_cascade/fields.py
@@ -3,5 +3,6 @@ from django.db.models import ForeignKey as FK
 class ForeignKey(FK):
     def __init__(self, *args, db_delete=False, **kwargs):
         self.db_delete = db_delete
+        kwargs.pop('db_delete', None)
         super().__init__(*args, **kwargs)
 
