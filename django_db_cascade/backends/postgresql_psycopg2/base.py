166df7f8c26bd29478c59b8c876fe0fb8170b68c
NickStefan <nickstefan12@gmail.com>
2017-06-15 13:04:26 -0700

    missing file name

--- django_db_cascade/backends/postgresql_psycopg2/base.py
+++ django_db_cascade/backends/postgresql_psycopg2/base.py
@@ -1,5 +1,5 @@
 from django.db.backends.postgresql_psycopg2.base import DatabaseWrapper as DBW
-from django_db_cascade.backends.postgresql_psycopg2 import DatabaseSchemaEditor
+from django_db_cascade.backends.postgresql_psycopg2.schema import DatabaseSchemaEditor
 
 class DatabaseWrapper(DBW):
     SchemaEditorClass = DatabaseSchemaEditor
