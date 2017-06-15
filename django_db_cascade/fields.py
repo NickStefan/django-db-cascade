15b66c85d0684c01a767ed6fcd562030858bad43
NickStefan <nickstefan12@gmail.com>
2017-06-15 14:06:56 -0700

    db_delete --> db_cascade

--- django_db_cascade/fields.py
+++ django_db_cascade/fields.py
@@ -1,8 +1,8 @@
 from django.db.models import ForeignKey as FK
 
 class ForeignKey(FK):
-    def __init__(self, *args, db_delete=False, **kwargs):
-        self.db_delete = db_delete
-        kwargs.pop('db_delete', None)
+    def __init__(self, *args, db_cascade=False, **kwargs):
+        self.db_cascade = db_cascade
+        kwargs.pop('db_cascade', None)
         super().__init__(*args, **kwargs)
 
