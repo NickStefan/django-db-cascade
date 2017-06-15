6dca9c9ad4dfa6bba11411cd67382cddabfd9099
NickStefan <nickstefan12@gmail.com>
2017-06-15 11:38:09 -0700

    db schema set up to work on custom or base foreign key

--- django_db_cascade/backends/postgresql_psycopg2/schema.py
+++ django_db_cascade/backends/postgresql_psycopg2/schema.py
@@ -1,8 +1,30 @@
 from django.db.backends.postgresql_psycopg2.schema import DatabaseSchemaEditor as DSE
 
 class DatabaseSchemaEditor(DSE):
+
+    sql_cascade = "ON DELETE CASCADE"
+
     sql_create_fk = (
         "ALTER TABLE %(table)s ADD CONSTRAINT %(name)s FOREIGN KEY (%(column)s) "
         "REFERENCES %(to_table)s (%(to_column)s)%(cascade)s%(deferrable)s"
     )
 
+    def _create_fk_sql(self, model, field, suffix):
+        from_table = model._meta.db_table
+        from_column = field.column
+        to_table = field.target_field.model._meta.db_table
+        to_column = field.target_field.column
+        suffix = suffix % {
+            "to_table": to_table,
+            "to_column": to_column,
+        }
+
+        return self.sql_create_fk % {
+            "table": self.quote_name(from_table),
+            "name": self.quote_name(self._create_index_name(model, [from_column], suffix=suffix)),
+            "column": self.quote_name(from_column),
+            "to_table": self.quote_name(to_table),
+            "to_column": self.quote_name(to_column),
+            "deferrable": self.connection.ops.deferrable_sql(),
+            "cascade": self.sql_cascade if getattr(field, 'db_cascade', False) else ""
+        }
