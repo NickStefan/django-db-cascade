48dcd286c1a3b1fb153c0de70e7679dc4ad115fc
NickStefan <nickstefan12@gmail.com>
2017-06-15 16:12:11 -0700

    debug

--- django_db_cascade/fields.py
+++ django_db_cascade/fields.py
@@ -3,5 +3,5 @@ from django.db.models import ForeignKey as FK
 class ForeignKey(FK):
     def __init__(self, to, on_delete, **kwargs):
         db_cascade = kwargs.pop('db_cascade', False)
-        super().__init__(to, on_delete, **kwargs)
         self.db_cascade = db_cascade
+        super().__init__(to, on_delete, **kwargs)
