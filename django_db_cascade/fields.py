43dff71ed6a3dd54867f7f2c37f938cacd749651
NickStefan <nickstefan12@gmail.com>
2017-06-16 16:37:16 -0700

    many2many doesnt work like that

--- django_db_cascade/fields.py
+++ django_db_cascade/fields.py
@@ -1,5 +1,5 @@
 from django.db.models import (
-    ForeignKey as FK, OneToOneField as OTO, ManyToManyField as MTM,
+    ForeignKey as FK, OneToOneField as OTO,
     DO_NOTHING
 )
 from django_db_cascade.deletions import DB_CASCADE
@@ -32,21 +32,6 @@ class OneToOneField(OTO):
 
     def deconstruct(self):
         name, path, args, kwargs = super(OneToOneField, self).deconstruct()
-        if self.on_delete_db_cascade:
-            kwargs['on_delete'] = DB_CASCADE
-        return name, path, args, kwargs
-
-class ManyToManyField(MTM):
-    def __init__(self, to, on_delete, **kwargs):
-        if on_delete == DB_CASCADE:
-            self.on_delete_db_cascade = True
-            on_delete = DO_NOTHING
-        else:
-            self.on_delete_db_cascade = False
-        super(ManyToManyField, self).__init__(to, on_delete, **kwargs)
-
-    def deconstruct(self):
-        name, path, args, kwargs = super(ManyToManyField, self).deconstruct()
         if self.on_delete_db_cascade:
             kwargs['on_delete'] = DB_CASCADE
         return name, path, args, kwargs
