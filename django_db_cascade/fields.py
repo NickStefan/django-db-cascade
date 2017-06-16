937535032948473fa4e6d2ab523a36a0e8f14136
NickStefan <nickstefan12@gmail.com>
2017-06-16 11:34:04 -0700

    one to one

--- django_db_cascade/fields.py
+++ django_db_cascade/fields.py
@@ -1,4 +1,5 @@
 from django.db.models import ForeignKey as FK
+from django.db.models import OneToOneField as OTO
 from django_db_cascade.deletions import DB_CASCADE
 
 class ForeignKey(FK):
@@ -11,3 +12,15 @@ class ForeignKey(FK):
         if self.on_delete_db_cascade:
             kwargs['on_delete'] = DB_CASCADE
         return name, path, args, kwargs
+
+class OneToOneField(OTO):
+    def __init__(self, to, on_delete, **kwargs):
+        self.on_delete_db_cascade = True if on_delete == DB_CASCADE else False
+        super().__init__(to, on_delete, **kwargs)
+
+    def deconstruct(self):
+        name, path, args, kwargs = super(ForeignKey, self).deconstruct()
+        if self.on_delete_db_cascade:
+            kwargs['on_delete'] = DB_CASCADE
+        return name, path, args, kwargs
+
