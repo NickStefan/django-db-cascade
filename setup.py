e267ef4771e7bee1626efedcd0ae587a7352c8e1
Jordan Kapelner <jordanka@optonline.net>
2018-04-21 12:20:07 -0400

    fixed psycopg2 dependency in setup.  ignore build files in git

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-db-cascade-2',
-    version='0.2',
+    version='0.2.2',
     description='Fork of django-db-cascade for Django 2.0.  Optionally use postgres db ON CASCADE DELETE on django foreign keys',
     url='http://github.com/jkapelner/django-db-cascade',
     author='Nick Stefan, Jordan Kapelner',
