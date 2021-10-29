14e9a1d7cb315ea4013b1a786ba5cf2b44ca7392
Jordan Kapelner <jordanka@optonline.net>
2021-10-28 23:43:31 -0400

    updated repo name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='django-db-cascade-2',
     version='0.3.0',
     description='Fork of django-db-cascade for Django 3.0.  Optionally use postgres db ON CASCADE DELETE on django foreign keys',
-    url='http://github.com/jkapelner/django-db-cascade',
+    url='http://github.com/jkapelner/django-db-cascade-2',
     author='Nick Stefan, Jordan Kapelner',
     author_email='nickstefan12@gmail.com',
     license='MIT',
