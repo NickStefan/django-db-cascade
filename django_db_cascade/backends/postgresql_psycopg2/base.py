6409f6e693d74011d101c38a41683d4dd16bf569
Jordan Kapelner <jordanka@optonline.net>
2021-10-29 00:22:45 -0400

    fixed import bug

--- django_db_cascade/backends/postgresql_psycopg2/base.py
+++ django_db_cascade/backends/postgresql_psycopg2/base.py
@@ -1,5 +1,5 @@
 from django.db.backends.postgresql.base import DatabaseWrapper as DBW
-from django_db_cascade.backends.postgresql.schema import DatabaseSchemaEditor
+from .schema import DatabaseSchemaEditor
 
 class DatabaseWrapper(DBW):
     SchemaEditorClass = DatabaseSchemaEditor
